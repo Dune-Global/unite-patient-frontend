63e1c59e36bfd0eadb98374e681e4c5529e37808
Lithara Perera <litharaperera2002@gmail.com>
2024-03-21 00:38:43 +0530

    refactor: Button loader

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
-
 import { cn } from "@/lib/utils";
+import { LucideLoaderCircle } from "lucide-react";
 
+// Define button variants
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
@@ -11,10 +12,9 @@ const buttonVariants = cva(
       variant: {
         default: "bg-ublue-100 text-ugray-0 hover:bg-ublue-50",
         outline:
-          "border border-ublue-200 bg-background text-ublue-200 hover:bg-ublue-200/90 hover:text-ublue-200/90",
-        icon: "bg-background text-ublue-200 hover:bg-ublue-200/90 hover:text-ublue-200/90",
+          "border border-ublue-200 hover:border-ublue-50 text-ublue-200 hover:text-ublue-50",
+        icon: " text-ublue-200 hover:bg-ublue-100 hover:text-ublue-100",
       },
-
       size: {
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-11 rounded-md px-6 text-sm",
@@ -28,24 +28,52 @@ const buttonVariants = cva(
   }
 );
 
+// Button props interface
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
+  loading?: boolean; // Add loading prop
 }
 
+// Button component
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    { className, variant, size, asChild = false, loading = false, ...props },
+    ref
+  ) => {
     const Comp = asChild ? Slot : "button";
+
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size, className }), {
+          "opacity-50 cursor-not-allowed": loading, // Disable button when loading
+        })}
         ref={ref}
+        disabled={loading} // Disable button when loading
         {...props}
-      />
+      >
+        <div className="relative">
+          <span className={loading ? "opacity-0" : ""}>{props.children}</span>
+          {loading && <Spinner />}
+        </div>
+      </Comp>
     );
   }
 );
 Button.displayName = "Button";
 
+// Spinner component
+const Spinner = () => (
+  <div className="absolute inset-0 flex items-center justify-center">
+    <LucideLoaderCircle className="w-5 h-5 animate-spin" />
+  </div>
+);
+
 export { Button, buttonVariants };
+
+
+
+
+
+
