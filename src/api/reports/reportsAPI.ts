39bf5441ca575f92e2ccabdbfa1cb1e0b89e57a2
yasithranusha <yasithranusha24@gmail.com>
2024-03-24 20:24:57 +0530

    feat: Dashboard image view

--- src/api/reports/reportsAPI.ts
+++ src/api/reports/reportsAPI.ts
@@ -3,8 +3,12 @@ import {
   GET_ALL_REPORTS_URL,
   GET_DOCTOR_LIST_AND_ACCESS_INFO_URL,
   UPDATE_REPORT_ACCESS_URL,
+  UPLOAD_REPORT_URL,
+  UPLOAD_FILE_URL,
 } from "@/api/_url/reports/url";
 import CustomAxios from "@/utils/axiosInstance";
+import axios from "axios";
+import exp from "constants";
 
 /**
  * Get all reports of a patient
@@ -57,4 +61,47 @@ export const updateReportAccess = async (
       ],
     },
   });
+};
+
+export const uploadReport = async (
+  reportName: string,
+  date: string,
+  picture: File
+) => {
+  // Create form data
+  const formData = new FormData();
+  formData.append("file", picture);
+
+  // Upload the file
+  return axios
+    .post(`${BACKEND_BASE_URL}/${UPLOAD_FILE_URL}`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+    .then((uploadResponse: any) => {
+      // Check if the upload was successful
+      if (uploadResponse.status === 201) {
+        // Get the URL from the response
+        const url = uploadResponse.data.imgUrl;
+
+        // Submit the form data
+        return CustomAxios({
+          method: "POST",
+          baseURL: BACKEND_BASE_URL,
+          url: UPLOAD_REPORT_URL,
+          data: {
+            reportType: reportName,
+            tookDate: date,
+            reportUrl: url,
+          },
+        });
+      } else {
+        console.error("Upload failed");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      throw error;
+    });
 };
